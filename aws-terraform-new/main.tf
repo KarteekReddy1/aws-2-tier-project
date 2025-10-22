285369b24d8fac6559ed86a23a95637c92c6f498
KarteekReddy1 <104560543+KarteekReddy1@users.noreply.github.com>
2025-10-22 22:52:12 +0530

    Update main.tf

--- aws-terraform-new/main.tf
+++ aws-terraform-new/main.tf
@@ -44,11 +44,11 @@ module "security_group" {
 
 module "ec2" {
   source             = "./ec2"
-  ami_id             = "ami-0bd4cda58efa33d23" # Ubuntu 22.04 (us-east-1)
+  ami_id             = "ami-02d26659fd82cf299" # Ubuntu 22.04 (us-east-1)
   instance_type      = "t3.micro"
   subnet_id          = element(module.vpc.public_subnet_ids, 0)
   security_group_ids = [module.security_group.sg_ec2_sg_ssh_http_id]
-  key_name           = "jenkins_sonar.pem"  # create keypair manually & update name here
+  key_name           = "jenkins.pem"  # create keypair manually & update name here
 }
 
 module "eks" {
@@ -77,6 +77,7 @@ module "rds" {
   instance_class          = "db.t3.micro"
   backup_retention_period = 0
 }
+
 
 
 
